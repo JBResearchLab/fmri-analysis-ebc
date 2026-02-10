fbbab3e329b59bc23788d82fe59a870a562a3a91
Melissa Thye <melissathye@gmail.com>
2026-02-10 10:47:18 +0000

    update search for smooth file for multiecho data

--- scripts/06.first_level/timecourse_pipeline.py
+++ scripts/06.first_level/timecourse_pipeline.py
@@ -114,9 +114,9 @@ def create_timecourse_workflow(sharedDir, projDir, derivDir, workDir, outDir, su
         # check to see whether outputs exist in smoothDir (if smoothDir was specified in config file)
         if smoothDir: 
             if splithalf_id != 0:
-                smooth_file = glob.glob(op.join(smoothDir, 'sub-{}'.format(sub), 'preproc', '{}_splithalf{}'.format(run_name, splithalf_id), '{}_space-{}*preproc_bold_smooth.nii.gz'.format(prefix, space_name)))[0]
+                smooth_file = glob.glob(op.join(smoothDir, 'sub-{}'.format(sub), 'preproc', '{}_splithalf{}'.format(run_name, splithalf_id), '{}_space-{}*_smooth.nii.gz'.format(prefix, space_name)))[0]
             else:
-                smooth_file = glob.glob(op.join(smoothDir, 'sub-{}'.format(sub), 'preproc', '{}'.format(run_name), '{}_space-{}*preproc_bold_smooth.nii.gz'.format(prefix, space_name)))[0]
+                smooth_file = glob.glob(op.join(smoothDir, 'sub-{}'.format(sub), 'preproc', '{}'.format(run_name), '{}_space-{}*_smooth.nii.gz'.format(prefix, space_name)))[0]
 
             if os.path.exists(smooth_file):
                 mni_file = smooth_file
